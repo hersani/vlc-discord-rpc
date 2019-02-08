00c0eef933bff302b1717ce5a9d2b6e5920873d3
hersani <46082901+hersani@users.noreply.github.com>
2019-02-08 13:18:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.vlcPass="rosebud"	//Lua HTTP password
+exports.vlcPass="Lua HTTP"	//Lua HTTP password
 exports.vlcPort=8080		//default value
 exports.vlcHost="localhost"	//shouldnt need to change but whatever
 //Start commands
